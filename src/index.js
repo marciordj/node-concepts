bca8206d1fcffb4883fbb0ed75d211113e011e66
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-08-29 11:27:19 -0300

    criar usuario

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const { v4: uuidV4 } = require("uuid");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello world ignite - testee" });
+app.use(express.json());
+
+const costumers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body; //pega os dados do body da requisição
+
+  const customerAlreadyExist = costumers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExist) {
+    return response
+      .status(400)
+      .json({ error: "Esse usuário já está cadastrado" });
+  }
+
+  costumers.push({
+    cpf,
+    name,
+    id: uuidV4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
