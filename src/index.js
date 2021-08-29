3b3e8f7cddb32414c1f91952bc838220b3d6c7b4
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-08-29 19:39:00 -0300

    refactor consult costumer

--- src/index.js
+++ src/index.js
@@ -5,22 +5,20 @@ const app = express();
 
 app.use(express.json());
 
-const costumers = [];
+const customers = [];
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body; //pega os dados do body da requisição
 
-  const customerAlreadyExist = costumers.some(
+  const customerAlreadyExist = customers.some(
     (customer) => customer.cpf === cpf
   );
 
   if (customerAlreadyExist) {
-    return response
-      .status(400)
-      .json({ error: "Esse usuário já está cadastrado" });
+    return response.status(400).json({ error: "Esse usuário já está cadastrado" });
   }
 
-  costumers.push({
+  customers.push({
     cpf,
     name,
     id: uuidV4(),
@@ -28,6 +26,19 @@ app.post("/account", (request, response) => {
   });
 
   return response.status(201).send();
+});
+
+app.get("/statement", (request, response) => {
+  const { cpf } = request.body;
+
+  const findCostumer = customers.find(customer => customer.cpf === cpf);
+
+  if (!findCostumer) {
+    return response.status(400).json({error: "Cliente não encontrado"})
+  }
+  
+  return response.json(findCostumer.statement)
+  
 });
 
 app.listen(3333);
