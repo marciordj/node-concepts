5c36359b516e47de44dce009397f3ea8b69db345
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-08-30 00:37:23 -0300

    feat: add new func

--- src/index.js
+++ src/index.js
@@ -7,6 +7,32 @@ app.use(express.json());
 
 const customers = [];
 
+const verifyIfExistAccount = (request, response, next) => {
+  const { cpf } = request.headers;
+
+  const findCustomer = customers.find(customer => customer.cpf === cpf);
+  
+  if (!findCustomer) {
+    return response.status(400).json({error: "Usuário não encontrado"});
+  }
+
+  request.customer = findCustomer;
+
+  return next();
+}
+
+const getBalance = (statement) => {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    }
+
+    return acc - operation.amount;
+  }, 0)
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body; //pega os dados do body da requisição
 
@@ -28,17 +54,51 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.body;
+// app.use(verifyIfExistAccount) usa assim se todas as rotas abaixo forem usar, se nao forem coloca o middlware em cada 1
 
-  const findCostumer = customers.find(customer => customer.cpf === cpf);
+app.get("/statement", verifyIfExistAccount, (request, response) => { //middleware coloca entre o endpoint e request e response
+  const { customer } = request;
 
-  if (!findCostumer) {
-    return response.status(400).json({error: "Cliente não encontrado"})
-  }
-  
-  return response.json(findCostumer.statement)
-  
+  return response.json(customer.statement)
 });
+
+app.post("/deposit", verifyIfExistAccount ,(request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const bankOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(bankOperation);
+
+
+  return response.status(201).send()
+});
+
+app.post("/withdraw", verifyIfExistAccount, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Saldo insuficiente" })
+  }
+
+  const bankOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  }
+
+  customer.statement.push(bankOperation);
+
+
+  return response.status(201).send()
+})
 
 app.listen(3333);
